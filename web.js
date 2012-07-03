182daa250aa64904ccb1775ea6f9e716d364cc3f
libra guest <topherchristie@gmail.com>
2012-07-03 14:46:20 -0400

    added express and kitty pictures

--- web.js
+++ web.js
@@ -1,9 +1,24 @@
 var express = require('express');
-
+var cons = require('consolidate');
+var swig = require('swig');
+swig.init({
+    root: __dirname + '/views'
+});
 var app = express();
 
+app.configure(function(){
+     app.set('view options', { layout: false });
+  app.engine('html',cons.swig);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'html');
+  app.use("/public",express.static(__dirname + '/public'));
+});
+
+
+
 app.get('/', function(req,res){
-	res.send('hello brian, this worked');
+	//res.send('hello brian, this worked');
+    res.render("index");
 });
 
 var port = process.env.PORT || 5000;
